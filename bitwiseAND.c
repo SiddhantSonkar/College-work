e69cda51e7a2527777edc5c93a5f7c7b4bc01d19
SiddhantSonkar <124968287+SiddhantSonkar@users.noreply.github.com>
2023-03-23 19:08:19 +0530

    Add files via upload

---
+++ bitwiseAND.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=2 , b=4,  c;
+
+	printf("%d",a&b);
+	printf("\n%d",a|b);
+	printf("\n%d",a^b);
+	return 0;
+}
---
+++ class2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	int b;
+    int a[5]={10,15,16,20,50};
+	scanf("%d",&b);
+	if(b>=0&&b<=5)
+	{
+		printf("%d", a[b]);
+	}
+	else if(b<0)
+	{
+		printf("Not Valid");
+	}
+	else
+	{
+		printf("Not Valid");2	
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
